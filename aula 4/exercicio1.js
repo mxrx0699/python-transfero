933001db0b49946b4b2139506082e73d5ff18a2e
Mariana <mxrx0699@gmail.com>
2025-01-27 14:27:57 -0300

    codigo atualizado

--- aula 4/exercicio1.js
+++
@@ -1,2 +0,0 @@
-/*ideia: um programa para receber um número de 1 a 7
-e informar qual é o dia da semana */
---
+++ aula4/exercicio1.js
@@ -0,0 +1,21 @@
+/*ideia: um programa para receber um número de 1 a 7
+e informar qual é o dia da semana 
+*/
+let dia = 3
+if (dia == 1) {
+    console.log ("Domingo")
+} else if (dia == 2) {
+    console.log("Segunda")
+} else if (dia == 3) {
+    console.log ("Terça")
+} else if (dia == 4) {
+    console.log ("Quarta")
+} else if (dia == 5) {
+    console.log("Quinta")
+} else if (dia == 6 ) {
+    console.log("Sexta")
+} else if (dia == 7) {
+    console.log ("Sábado")
+} else {
+    console.log ("Dia inválido. Informe um número de 1 a 7.")
+} 
