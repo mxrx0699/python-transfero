ed0bff9c7b06fb33a90c7216d0de488c3eb4aa13
Mariana <mxrx0699@gmail.com>
2025-01-27 15:58:11 -0300

    update código

--- aula4/idade.js
+++ aula4/idade.js
@@ -1,17 +1,35 @@
-let idade = 12
-switch(idade) {
-    case (idade < 12):
-        console.log("Criança")
-        break
-    case (idade >= 12):
-        console.log("Adolescente")
-        break
-    case (idade >= 18):
-        console.log("Adulto")
-        break
-    case (idade >= 65):
-        console.log ("Idoso")
-        break
+// let idade = 12
+// switch(idade) {
+//     case (idade < 12):
+//         console.log("Criança")
+//         break
+//     case (idade >= 12):
+//         console.log("Adolescente")
+//         break
+//     case (idade >= 18):
+//         console.log("Adulto")
+//         break
+//     case (idade >= 65):
+//         console.log ("Idoso")
+//         break
+//     default:
+//         console.log("Informe uma idade!")
+// }
+
+const idade = 20;
+switch(true) {
+    case (idade >= 0 && idade <= 12):
+        console.log("Criança");
+        break;
+    case (idade >= 13 && idade <= 17):
+        console.log("Adolescente");
+        break;
+    case (idade >= 18 && idade <= 65):
+        console.log("Adulto");
+        break;
+    case (idade >= 66):
+        console.log("Idoso");
+        break;
     default:
-        console.log("Informe uma idade!")
+        console.log("Idade inválida");
 }
