169e0d64b1ba72c118ee9f879c71d0ecf847369c
Mariana <mxrx0699@gmail.com>
2025-01-29 16:57:26 -0300

    update

--- aula5/arquivo1.js
+++ aula5/arquivo1.js
@@ -110,12 +110,12 @@
 
 //exercicio 7 - Solicita três números e calcula a média.
 
-let valor = parseFloat(prompt("Digite o primeiro valor:"));
-let valor2 = parseFloat(prompt("Digite o segundo valor:"));
-let valor3 = parseFloat(prompt("Digite o terceiro valor:"));
-if(isNaN(valor) || isNaN(valor2) || isNaN(valor3)) {
-    alert("Digite um número válido!")
-} else {
-    let media = (valor + valor2 + valor3) / 3 
-    document.write("O valor da média é: "+media.toFixed(2))
-}
+// let valor = parseFloat(prompt("Digite o primeiro valor:"));
+// let valor2 = parseFloat(prompt("Digite o segundo valor:"));
+// let valor3 = parseFloat(prompt("Digite o terceiro valor:"));
+// if(isNaN(valor) || isNaN(valor2) || isNaN(valor3)) {
+//     alert("Digite um número válido!")
+// } else {
+//     let media = (valor + valor2 + valor3) / 3 
+//     document.write("O valor da média é: "+media.toFixed(2))
+// }
